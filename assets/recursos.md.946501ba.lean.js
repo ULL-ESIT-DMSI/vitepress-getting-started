e58a84e905d763935b01a212824212e61d085b0d
crguezl <crguezl@users.noreply.github.com>
2022-09-13 11:44:48 +0000

    Auto deploy from Github Actions

---
+++ assets/recursos.md.946501ba.lean.js
@@ -0,0 +1,10 @@
+import{P as a}from"./chunks/plinfo.b98f57c3.js";import{o as i,c as o,a as e,t,d as r,f as s,e as u}from"./app.ab3307f0.js";const l=e("h2",{id:"recursos-ull",tabindex:"-1"},[s("Recursos ULL "),e("a",{class:"header-anchor",href:"#recursos-ull","aria-hidden":"true"},"#")],-1),n=u("",14),h=e("li",null,[e("a",{href:"https://groups.google.com/u/1/a/ull.edu.es/g/asignatura_139263121",target:"_blank",rel:"noreferrer"},"Google group de PL ")],-1),c=["href"],_=JSON.parse('{"title":"Recursos","description":"","frontmatter":{"title":"Recursos"},"headers":[],"relativePath":"recursos.md","lastUpdated":1663069421000}'),d={name:"recursos.md"},G=Object.assign(d,{setup(p){return(g,b)=>(i(),o("div",null,[l,e("pre",null,""+t(r(a).author.name)+`
+
+`+t(r(a).campus_virtual)+`
+
+`+t(r(a).participantes)+`
+
+`+t(r(a).calificador)+`
+
+`+t(r(a).tareas)+`
+`,1),n,e("ul",null,[h,e("li",null,[e("a",{href:r(a).google_plus},"Comunidad Currents/Google+ PL",8,c)])])]))}});export{_ as __pageData,G as default};
